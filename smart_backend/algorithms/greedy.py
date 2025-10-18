e8c0da4bbb17a67fe1861d762ada5e940a9b8888
Ivan Ausecha <ivanausecha5@gmail.com>
2025-10-18 11:31:22 -0500

    Add algorithm skeleton files
    - Create bfs.py for Breadth-First Search
    - Create dfs.py for Depth-First Search
    - Create uniform_cost.py for Uniform Cost Search
    - Create greedy.py for Greedy Best-First Search
    - Create astar.py for A* Search
    - All algorithms have solve() function structure ready for implementation
    - Include documentation and parameter specifications

---
+++ smart_backend/algorithms/greedy.py
@@ -0,0 +1,28 @@
+"""
+Greedy Best-First Search Algorithm
+Busqueda voraz que utiliza heuristica para encontrar soluciones
+"""
+
+def solve(params: dict):
+    """
+    Ejecuta el algoritmo Greedy Best-First Search
+    
+    Args:
+        params: Diccionario con parametros del problema
+               - map: Mapa/grafo a resolver
+               - start: Nodo inicial
+               - goal: Nodo objetivo
+               - heuristic: Funcion heuristica a utilizar
+    
+    Returns:
+        dict: Resultado con el camino encontrado y estadisticas
+    """
+    # TODO: Implementar logica del algoritmo Greedy
+    
+    return {
+        "path": [],
+        "nodes_expanded": 0,
+        "cost": 0,
+        "max_depth": 0,
+        "message": "Algoritmo Greedy - Pendiente de implementacion"
+    }
